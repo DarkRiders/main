32534be0a22705f3834fad1e9e5be0ff84001dea
leoworm <leo.charles.p@gmail.com>
2016-02-16 20:46:25 +0100

    Added JQuery

--- js/main.js
+++ js/main.js
@@ -7,12 +7,16 @@ function getElement() {
 function restart() {
   delete localStorage.money;
 }
-
+if (localStorage.money==undefined) {
+  goal = prompt('Goal?');
+  setTimeout(function(){this.reload();}, 5000);
+}
 function clickHandler() {
   money = localStorage.getItem('money')
+  localStorage.setItem('money', goal);
   if (localStorage.money) {
-            localStorage.money = Number(localStorage.money) + Number(output.value);
+            localStorage.money = Number(localStorage.money) - Number(output.value);
         } else {
-            localStorage.money = 1;
+            localStorage.money = Number(goal);
         }
 }
